3a39850093a8d5e2459bda51a6826071430cc5c3
Nguyễn Lê Gia Bảo <154073233+gbaooo2407@users.noreply.github.com>
2025-05-31 10:47:50 +0700

    new file

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# simple-interest.sh
+
+echo "Enter principal:"
+read p
+echo "Enter rate of interest:"
+read r
+echo "Enter time (in years):"
+read t
+
+si=$(echo "scale=2; ($p * $r * $t) / 100" | bc)
+echo "Simple Interest = $si"
